c02181f10caedd506f84e43e1c673450e22d304f
Jayashree <pedini14sneha@gmail.com>
2024-11-14 21:51:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Task {
+  id: number;
+  title:string;
+  description:string
+}
+
+const App:React.FC = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
